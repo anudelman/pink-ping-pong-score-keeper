ba467b410b23d7fffb29d99c5267c75423b1ec5b
anudelman <anudelman@users.noreply.github.com>
2023-08-27 22:12:10 -0500

    Updated snake casing to camel casing and added a mini case study in Readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ping_pong_score_keeper
+# Pink Ping Pong Score Keeper
+
+## Project Name: Ping Pong Score Keeper
+
+## Objective: Develop a simple web application that helps users keep track of scores during a ping pong match.
+
+## Features:
+
+- Display the current score of each player.
+- Buttons to increase the score for each player.
+- A button to reset the game.
+- A dropdown to change the maximum score.
+
+### A simple Ping Pong Score Keeper application using HTML, CSS, and JavaScript. I wanted to challenge myself to create a basic interface, style it, and implement interactive functionality for score tracking and resetting. In the future I might enhance this project by adding features like win detection as a UI elemnt (e.g. - A dialog VS currently in console.log), additional micro-interactions, theming options, adding player names, scoreboard history and more.
--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const container = document.querySelector('#container');
 const hero = document.querySelector('#hero img')
-let p1_score_text = document.querySelector('span#p1_score');
-let p2_score_text = document.querySelector('#p2_score');
-const p1_btn = document.querySelector('#controls button:first-child');
-const p2_btn = document.querySelector('#controls button:last-child');
-const playing_to = document.querySelector('#playing_to_select');
+let p1ScoreText = document.querySelector('span#p1Score');
+let p2ScoreText = document.querySelector('#p2Score');
+const p1Btn = document.querySelector('#controls button:first-child');
+const p2Btn = document.querySelector('#controls button:last-child');
+const playingToSelect = document.querySelector('#playingToSelect');
 const reset_btn = document.querySelector('#controls button:nth-child(2)');
 
-let p1_score = 0
-let p2_score = 0
+let p1Score = 0
+let p2Score = 0
 // let game_in_progress = true;
 
-let playing_to_value = Number(playing_to.value);
-console.log(playing_to_value);
+let playingToValue = Number(playingToSelect.value);
+console.log(playingToValue);
 
 const add_shrink = () => {
     hero.classList.add('shrink');
-}
-
-const remove_shrink = () => {
-    hero.classList.remove('shrink');
 }
 
 // window.setTimeout(() => {
@@ -36,60 +32,60 @@ container.addEventListener('mouseout', () => {
 
 
 const reset_game = () => {
-    playing_to_value = Number(playing_to.value);
-    p1_score = 0;
-    p1_score_text.textContent = p1_score;
-    p2_score = 0;
-    p2_score_text.textContent = p2_score;
-    p1_btn.addEventListener('click', p1_click_handler);
-    p2_btn.addEventListener('click', p2_click_handler);
+    playingToValue = Number(playingToSelect.value);
+    p1Score = 0;
+    p1ScoreText.textContent = p1Score;
+    p2Score = 0;
+    p2ScoreText.textContent = p2Score;
+    p1Btn.addEventListener('click', p1_click_handler);
+    p2Btn.addEventListener('click', p2_click_handler);
     console.log("Resetting game");
 }
 
 ////// This game logic works, but seems strange from user perspective. /////////
 
-// const playing_to_handler = () => {
-//     playing_to_value = Number(playing_to.value);
+// const playingTo_handler = () => {
+//     playingToValue = Number(playingTo.value);
 // }
-// playing_to.addEventListener('change', playing_to_handler);
+// playingTo.addEventListener('change', playingTo_handler);
 
-playing_to.addEventListener('change', reset_game);
+playingToSelect.addEventListener('change', reset_game);
 reset_btn.addEventListener('click', reset_game);
 
 
-// Setting a flag is another approach to determining if the game is won or not e.g. - *** game_in_progress = true ***. set it to false if p2_score || p1_score is equal to or greater than the selected option (e.g. - 5);
+// Setting a flag is another approach to determining if the game is won or not e.g. - *** game_in_progress = true ***. set it to false if p2Score || p1Score is equal to or greater than the selected option (e.g. - 5);
 
 
 // always create an event handler, so you can use it for other things down the road. It also improves readibility
 const p1_click_handler = () => {
-    p1_score += 1;
-    if (p1_score >= playing_to_value) {
-        p2_btn.removeEventListener('click', p2_click_handler);
-        p1_btn.removeEventListener('click', p1_click_handler);
+    p1Score += 1;
+    if (p1Score >= playingToValue) {
+        p2Btn.removeEventListener('click', p2_click_handler);
+        p1Btn.removeEventListener('click', p1_click_handler);
         console.log("p1 wins!");
-        p1_score_text.textContent = p1_score;
+        p1ScoreText.textContent = p1Score;
     } else {
-        p1_score_text.textContent = p1_score;
+        p1ScoreText.textContent = p1Score;
     }
 }
 
 // always create an event handler, so you can use it for other things down the road. It also improves readibility
 const p2_click_handler = () => {
-    p2_score += 1;
-    if (p2_score >= playing_to_value) {
-        p2_btn.removeEventListener('click', p2_click_handler);
-        p1_btn.removeEventListener('click', p1_click_handler);
+    p2Score += 1;
+    if (p2Score >= playingToValue) {
+        p2Btn.removeEventListener('click', p2_click_handler);
+        p1Btn.removeEventListener('click', p1_click_handler);
         console.log("p2 wins!");
-        p2_score_text.textContent = p2_score;
+        p2ScoreText.textContent = p2Score;
     } else {
-        p2_score_text.textContent = p2_score;
+        p2ScoreText.textContent = p2Score;
     }
 
 }
 
 
-p1_btn.addEventListener('click', p1_click_handler);
-p2_btn.addEventListener('click', p2_click_handler);
+p1Btn.addEventListener('click', p1_click_handler);
+p2Btn.addEventListener('click', p2_click_handler);
 
 
 
